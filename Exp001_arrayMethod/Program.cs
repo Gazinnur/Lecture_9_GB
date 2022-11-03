0d1b0089505ecfafa67e26d72692c0eaa4080b33
gazinnur <gazinnur@mail.ru>
2022-11-03 23:38:40 +0300

    замена текста с помощью функций

---
+++ Exp001_arrayMethod/Program.cs
@@ -0,0 +1,70 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор... Газинур");
+}
+//Method1();
+// Вид 2
+void Method2(String msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2("Я молодец");
+
+void Method21(String msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+// Method21("Я молодец", 4);
+
+
+// Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+
+// Вид 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string mess = Method4(10, "235 ");
+//Console.WriteLine(mess);
+//Console.WriteLine("Hello, World!");
+
+string Method41(int count, string text)
+{
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+//string mess1 = Method41(10, "22 ");
+//Console.WriteLine(mess1);
+
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i}*{j}={i * j}");
+    }
+    Console.WriteLine();
+}
---
+++ Exp002_changeText/Program.cs
@@ -0,0 +1,28 @@
+﻿// работа с текстом
+// Дан текст, в этом тексте надо все пробелы заменить на черточки 
+// маленькие буквы "к" заменить большими буквами "К"
+// а большие буквы "С" заменить на маленькие буквы "с"
+
+string text = "Я думаю,- сказал князь, улыбаясь,-что ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне час?";
+
+string ChangeText(string text, char oldVal, char newVal)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldVal) result = result + $"{newVal}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string NEWtext = ChangeText(text, ' ', '|');
+Console.WriteLine(NEWtext);
+Console.WriteLine();
+ NEWtext = ChangeText(NEWtext, 'к', 'К');
+Console.WriteLine(NEWtext);
+Console.WriteLine();
+ NEWtext = ChangeText(NEWtext, 'С', 'с');
+Console.WriteLine(NEWtext);
+Console.WriteLine();
