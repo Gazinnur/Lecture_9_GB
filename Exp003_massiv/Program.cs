33ac633b48269fc9fd3a5b2a89b9783d4595842c
gazinnur <gazinnur@mail.ru>
2022-11-04 00:02:18 +0300

    сортировка чисел от мин к макс

---
+++ Exp003_massiv/Program.cs
@@ -0,0 +1,34 @@
+﻿// сортировка от минимума к максимуму
+
+int[] Text = { 1, 2, 5, 6, 8, 1, 2, 6 };
+
+void PrintText(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Hello, World!");
+
+void SelectionSort(int[] Arr)
+{
+    for (int i = 0; i < Arr.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < Arr.Length; j++)
+        {
+            if (Arr[j] < Arr[minPosition]) minPosition = j;
+        }
+        int temPorary = Arr[i];
+        Arr[i] = Arr[minPosition];
+        Arr[minPosition] = temPorary;
+    }
+}
+
+PrintText(Text);
+SelectionSort(Text);
+
+PrintText(Text);
